8ac735c7f410de6b0ba35db50c6ccd97e3d72506
chmilblick <92372083+chmilblick@users.noreply.github.com>
2026-04-09 02:01:13 +0200

    adding some console

--- pages/5_osexplo.py
+++ pages/5_osexplo.py
@@ -2,10 +2,7 @@
 import subprocess
 import streamlit as st
 
-cmd = st.text_input(
-    "commands to execute",
-    value="ls -arl; pwd"
-)
+cmd = st.text_input("OS commands to execute",value="")
 
 answ = st.empty()
 answ.text_area(label="output")
@@ -13,11 +10,4 @@ answ.text_area(label="output")
 if cmd:
    # output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read()
     output = subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
-    
-
-
-    #output = str(output).replace('\\r\\n','  \\n\\n')
-    #output="<pre>\"+"b"
-
-    answ.text_area(label="output",value=output,)
-    #answ.markdown(output)
+    answ.text_area(label="output",value=output)
